c2302561eb921d4cc6986f68589954420db9f98c
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-06-13 08:40:26 +0200

    delete

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ app.listen(PORT, () => {
     console.log(`Server running in port : http://localhost:${PORT}`);
 });
 
-//conexion con la BD
 async function getConnection() {
-    //SE CREA LA CONEXION
     const conex = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
@@ -28,7 +26,6 @@ async function getConnection() {
     return conex;
 }
 
-// endpoint que busca cocktails
 app.get('/cocktails', async (req, res) => {
     try {
         const conn = await getConnection();
@@ -38,7 +35,6 @@ app.get('/cocktails', async (req, res) => {
 
         let selectCocktails = 'SELECT * FROM cocktails';
 
-        // Si nombreFilterParams esta definido, modifica la query
         if (nombreFilterParams) {
             selectCocktails += ' WHERE nombre like ?';
         }
@@ -47,8 +43,8 @@ app.get('/cocktails', async (req, res) => {
         const [result] = await conn.query(selectCocktails, ['%' + nombreFilterParams + '%']);
 
         res.status(200).json({
-            info: { count: result.length }, // número de elementos
-            results: result, // listado
+            info: { count: result.length }, 
+            results: result, 
         });
         await conn.end();
     } catch (error) {
@@ -56,14 +52,13 @@ app.get('/cocktails', async (req, res) => {
     }
 });
 
-// buscar un cocktail por id
+
 app.get('/cocktails/:id', async (req, res) => {
     try {
-        //const id  = req.params.id;
         const { id } = req.params;
         const conn = await getConnection();
         const select = 'select * from cocktails where id = ?';
-        const [result] = await conn.query(select, [id]); // id lo obtuve del req.params
+        const [result] = await conn.query(select, [id]); 
 
         if (result.length === 0) {
             res.status(400).json({ message: 'El id no existe en la BD' });
@@ -75,7 +70,7 @@ app.get('/cocktails/:id', async (req, res) => {
     }
 });
 
-//añadir un nuevo cocktail
+
 app.post('/cocktails', async (req, res) => {
     const conn = await getConnection();
     const { nombre, ingredientes, autor, precio } = req.body;
@@ -90,13 +85,13 @@ app.post('/cocktails', async (req, res) => {
     ]);
     res.status(200).json({
         success: true,
-        id: nuevoCocktail.insertId, // id que generó MySQL para la nueva fila
+        id: nuevoCocktail.insertId, 
     });
 
     await conn.end();
 });
 
-//modificar un coktail por id
+
 app.put('/cocktails/:id', async (req, res) => {
     const conn = await getConnection();
     const idCocktail = req.params.id;
@@ -119,7 +114,7 @@ app.put('/cocktails/:id', async (req, res) => {
 });
 
 
-//delete un cocktail por id
+
 app.delete('/cocktails/:id', async (req, res) => {
     const conn = await getConnection();
     const idCocktail = req.params.id;
